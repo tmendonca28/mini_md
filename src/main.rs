e72958d9f998f42ebc1ed1d0974e1b6489cc7917
tmendonca28 <tmendonca28@gmail.com>
2020-09-21 23:08:31 +0300

    Adds variable and function returns
    Creating a function without errors
    Creating an integer var and assigning value of function to it
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+fn get_version() -> u16 {
+    2828
+}
+
 fn usage() {
+    let the_version : u16;
+    the_version = get_version();
     println!("MiniMD, a markdown compiler written by Anthony Mendonca");
+    println!("Version {}", the_version);
 }
 
 fn main() {
