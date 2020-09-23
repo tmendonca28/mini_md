5d845a00f811fff91f8e323caa626aaf66c1dd6e
tmendonca28 <tmendonca28@gmail.com>
2020-09-23 16:55:19 +0300

    Adds parse_markdown_file
    Reads file line-by-line
    Writes to a file with extension using push_str
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::io::Write;
+
 fn get_title() -> String {
     let mut the_title = String::from(env!("CARGO_PKG_NAME"));
     the_title.push_str(" (v");
@@ -10,6 +15,66 @@ fn get_title() -> String {
 fn parse_markdown_file(filename: &str) {
     print_short_banner();
     println!("[ INFO ] Trying to parse {}...", filename);
+
+    let input_filename = Path::new(filename);
+
+    let file = File::open(&input_filename)
+                .expect("[ ERROR ] Failed to open file!");
+
+    let mut _ptag: bool = false;
+    let mut _htag: bool = false;
+
+    let mut tokens: Vec<String> = Vec::new();
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line_contents = line.unwrap();
+        let mut first_char: Vec<char> = line_contents.chars().take(1).collect();
+        let mut output_line = String::new();
+        match first_char.pop() {
+            Some('#') => {
+              if _ptag {
+                _ptag = false;
+                output_line.push_str("</p>\n"); // adding \n for instructional clarity
+              } 
+              if _htag {
+                _htag = false;
+                output_line.push_str("</h1>\n"); // close it if we're already open
+              }
+        
+              _htag = true;
+              output_line.push_str("<h1>");
+              output_line.push_str(&line_contents[2..]); // Get all but the first two characters
+            },
+        
+            _ => {
+              
+              if !_ptag {
+                _ptag = true;
+                output_line.push_str("<p>");
+              } 
+        
+              output_line.push_str(&line_contents);
+              
+            }
+        };
+        if output_line != "<p></p>\n" {
+          tokens.push(output_line);
+        }
+    }
+    for t in &tokens {
+      println!("{}", t);
+    }
+    let mut output_filename = String::from(&filename[..filename.len()-3]);
+    output_filename.push_str(".html");
+    let mut outfile = File::create(output_filename)
+                      .expect("[ ERROR ] Could not create output file!");
+    for line in &tokens {
+      outfile.write_all(line.as_bytes())
+        .expect("[ ERROR ] Could not write to output file!");
+    }
+    println!("[ INFO ] Parsing complete!");
 }
 
 fn print_short_banner() {
---
+++ test.md
@@ -0,0 +1,11 @@
+# Where I stay?
+
+Nairobi, Kenya.
+
+# Where I have studied?
+
+University of Nairobi and Queen's University of Belfast.
+
+# Interests
+
+Software Engineering, Security Engineering, leveraging deep learning in Cyber Security, Blockchains, security tooling
